2f0d089ac3acb860f49a2088c6b42c334e67e43f
BenjaminSzodo <benjaminszodo@gmail.com>
2023-10-16 18:25:07 -0300

    feat/navbar: create NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import logo from "../../images/LogoResto.png";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center bg-red-950">
+      <div className="flex items-center">
+        <img src={logo} alt="LogoResto" className="h-40 w-40" />
+      </div>
+      <div className="hidden md:flex space-x-5 mr-20">
+        <button className="text-custom-orange hover:text-orange-200 text-lg uppercase font-bold">Promos</button>
+        <button className="text-custom-orange hover:text-orange-200 text-lg uppercase font-bold">Menu</button>
+        <button className="text-custom-orange hover:text-orange-200 text-lg uppercase font-bold">Contacto</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        'custom-orange': 'rgb(255, 181, 0)',
+      },
+    },
   },
   plugins: [],
 }
